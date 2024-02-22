66e321e4963874a10dd8c90f429e0598ea144945
Gautham Suresh <gauthamsureshhh@gmail.com>
2024-02-22 18:00:13 +0530

    final

--- medicals/settings.py
+++ medicals/settings.py
@@ -125,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'medicals',#Name of the database created for this project
+        'NAME': 'enma_med',#Name of the database created for this project
         'USER': 'root',#Enter your mysql username
         'PASSWORD': '',#Enter your mysql password
         'HOST': 'localhost',
